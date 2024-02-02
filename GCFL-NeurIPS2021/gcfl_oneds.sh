57edd65228c594525493c51f3878e96d5b0fc922
Junhao Li <128126552+Lijunhaohz@users.noreply.github.com>
2024-02-02 19:17:26 +0000

    add .sh script

---
+++ GCFL-NeurIPS2021/gcfl_oneds.sh
@@ -0,0 +1,13 @@
+client_=10
+seed_=10
+seq_len_=10
+
+nohup python main_oneDS.py \
+    --repeat 1 \
+    --data_group 'PROTEINS' \
+    --num_clients 10 \
+    --seed 10  \
+    --epsilon1 0.03 \
+    --epsilon2 0.06 \
+    --seq_length 10 \
+    > logs/PROTEINS_client_${client_}_seed_${seed_}_seq_${seq_len_}.log 2>&1 &
